d61ff6b0e4cb64d8c2b52e2789fcfcb2eafef08f
DESKTOP-P5C09A5\TND195 <truongnhudung95@gmail.com>
2018-05-28 22:42:22 +0700

    Thay doi layout category

--- app/src/main/java/com/example/admin/cardview_demo/MainActivity.java
+++ app/src/main/java/com/example/admin/cardview_demo/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.admin.cardview_demo;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -54,8 +59,22 @@ public class MainActivity extends AppCompatActivity {
         categoryList.add(category);
         category = new Category(R.drawable.ic_technology,"1", "Technology");
         categoryList.add(category);
+        category = new Category(R.drawable.ic_technology,"1", "Technology");
+        categoryList.add(category);
+        category = new Category(R.drawable.ic_technology,"1", "Technology");
+        categoryList.add(category);
+        category = new Category(R.drawable.ic_technology,"1", "Technology");
+        categoryList.add(category);
+        category = new Category(R.drawable.ic_technology,"1", "Technology");
+        categoryList.add(category);
+        category = new Category(R.drawable.ic_technology,"1", "Technology");
+        categoryList.add(category);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
         rv_Category.setAdapter(categoryAdapter);
-        rv_Category.setLayoutManager(new LinearLayoutManager(this));
+        rv_Category.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+        rv_Category.setItemAnimator(new DefaultItemAnimator());
+        rv_Category.setLayoutManager(mLayoutManager);
 
 
 
@@ -74,12 +93,55 @@ public class MainActivity extends AppCompatActivity {
 
         categoryAdapter.notifyDataSetChanged();
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
     }
-    private void prepareMovieData() {
 
+    /**
+     * RecyclerView item decoration - give equal margin around grid item
+     */
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        private GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 
 }
